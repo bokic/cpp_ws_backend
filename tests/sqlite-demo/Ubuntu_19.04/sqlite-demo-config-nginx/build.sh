9698195ceac42c75eaec4be7b84697465323ad7d
Boris Barbulovski <bbarbulovski@gmail.com>
2019-10-24 07:59:55 +0200

    Fix ubuntu nginx site config path.

--- tests/sqlite-demo/Ubuntu_19.04/sqlite-demo-config-nginx/build.sh
+++ tests/sqlite-demo/Ubuntu_19.04/sqlite-demo-config-nginx/build.sh
@@ -8,11 +8,11 @@ rm -f ${PROJECT_NAME}_*.deb
 
 rm -rf ${PROJECT_NAME}_*
 
-mkdir -p ${PROJECT_DIR}/etc/nginx/sites-enabled
+mkdir -p ${PROJECT_DIR}/etc/nginx/sites-available
 mkdir -p ${PROJECT_DIR}/DEBIAN
 
 cp DEBIAN/* ${PROJECT_DIR}/DEBIAN
-cp 000-sqlite-demo.com ${PROJECT_DIR}/etc/nginx/sites-enabled
+cp 000-sqlite-demo.com ${PROJECT_DIR}/etc/nginx/sites-available
 
 sed -i -e "s/{VERSION}/${VERSION}/g" ${PROJECT_DIR}/DEBIAN/control
 
