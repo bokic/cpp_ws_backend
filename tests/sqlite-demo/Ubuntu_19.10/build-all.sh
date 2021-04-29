3dad87d3860a2ec5dd4adb368ebb3d163831d7dc
Boris Barbulovski <bbarbulovski@gmail.com>
2021-04-29 21:55:47 +0200

    Remove Ubuntu 19.04 and 19.10, and add Ubuntu 20.04 LTS

--- tests/sqlite-demo/Ubuntu_19.10/build-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-rm -f *.deb
-
-cd sqlite-demo              ;./build.sh;mv *.deb ..;cd ..
-cd sqlite-demo-config-nginx ;./build.sh;mv *.deb ..;cd ..
-cd sqlite-demo-fe           ;./build.sh;mv *.deb ..;cd ..
