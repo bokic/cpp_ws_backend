c6892fd2b69eac309257e155ee107d40d202b575
Boris Barbulovski <bbarbulovski@gmail.com>
2021-04-30 13:25:32 +0200

    Simplify Ubuntu 20.04 build-all.sh script

--- tests/sqlite-demo/Ubuntu_20.04/build-all.sh
+++ tests/sqlite-demo/Ubuntu_20.04/build-all.sh
@@ -2,6 +2,6 @@
 
 rm -f *.deb
 
-cd sqlite-demo              ;./build.sh;mv *.deb ..;cd ..
-cd sqlite-demo-config-nginx ;./build.sh;mv *.deb ..;cd ..
-cd sqlite-demo-fe           ;./build.sh;mv *.deb ..;cd ..
+(cd sqlite-demo              ;./build.sh;mv *.deb ..)
+(cd sqlite-demo-config-nginx ;./build.sh;mv *.deb ..)
+(cd sqlite-demo-fe           ;./build.sh;mv *.deb ..)
