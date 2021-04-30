5dff01c265b971d644e4fcd9b1c2128bb8e6bb47
Boris Barbulovski <bbarbulovski@gmail.com>
2021-04-30 21:25:23 +0200

    Update sqlite-demo.service path.

--- tests/sqlite-demo/Ubuntu_20.04/sqlite-demo/build.sh
+++ tests/sqlite-demo/Ubuntu_20.04/sqlite-demo/build.sh
@@ -18,7 +18,7 @@ mkdir -p ${PROJECT_DIR}/DEBIAN
 cp DEBIAN/* ${PROJECT_DIR}/DEBIAN
 cp ../../sqlite-demo ${PROJECT_DIR}/opt/${PROJECT_NAME}
 cp ../../chinook.db ${PROJECT_DIR}/opt/${PROJECT_NAME}
-cp ../../sqlite-demo.service ${PROJECT_DIR}/etc/systemd/system
+cp ../../sqlite-demo.service ${PROJECT_DIR}/lib/systemd/system
 
 sed -i -e "s/{VERSION}/${VERSION}/g" ${PROJECT_DIR}/DEBIAN/control
 sed -i -e "s/{ARCH}/${ARCH}/g" ${PROJECT_DIR}/DEBIAN/control
